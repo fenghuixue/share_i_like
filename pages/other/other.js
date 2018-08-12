04f095af59019d8977a8cd2ca3014749c5713635
张倩 <zq6@meitu.com>
2018-08-12 17:37:38 +0800

    城市定向

--- pages/other/other.js
+++ pages/other/other.js
@@ -1,11 +1,14 @@
 // pages/other/other.js
+import {cityConf, cityObj} from './area.js';
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+     areaId: 3,
+     cityMenu: cityConf[3]
   },
 
   /**
@@ -21,46 +24,10 @@ Page({
   onReady: function () {
   
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  checkboxChange: function(e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
   },
+  checkAll() {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
   }
 })
